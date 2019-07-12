b0aea259139df99c7dceacbbf9bd1f1d7d4fd753
stl-021 <chirag.s@ivymobility.com>
2019-07-12 12:59:51 +0530

    ->permission check move to bloc.

--- example/lib/bloc/contact_bloc.dart
+++ example/lib/bloc/contact_bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter_plugin_example/model/contact.dart';
 import 'package:flutter_plugin/flutter_plugin.dart';
+import 'package:simple_permissions/simple_permissions.dart';
 
 class ContactBloc {
   StreamController<List<Contact>> contactStreamController = StreamController();
@@ -9,7 +10,25 @@ class ContactBloc {
     return contactStreamController.stream;
   }
 
-  void getContacts() async {
+  ContactBloc(){
+    _checkPermission();
+  }
+
+  void _checkPermission() async {
+    var isGranted =
+    await SimplePermissions.checkPermission(Permission.ReadContacts);
+    if (!isGranted) {
+      var result =
+      await SimplePermissions.requestPermission(Permission.ReadContacts);
+      if (result == PermissionStatus.authorized) {
+        _getContacts();
+      }
+    }else{
+      _getContacts();
+    }
+  }
+
+  void _getContacts() async {
     List<Map<dynamic, dynamic>> contactMaps =
         await FlutterPlugin.getContentValue(
             "content://com.android.contacts/data/");
